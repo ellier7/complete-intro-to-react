5bb0feaecdb2961acda72e61e5cd891a015d9b1e
ellier7 <eroussopoulos@gmail.com>
2016-11-11 20:01:13 -0500

    MyTitle file added

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,22 +1,14 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
-	var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-	render () {
-		return (
-			div(null,
-				h1({style: {color: this.props.color}}, this.props.title)
-				)
-			)
-	}
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 
-	var MyFirstComponent = (
-		div(null, MyTitleFact({title: "Props are great!", color: "papayawhip"})
-			)
-		)
-
+var MyFirstComponent = (
+  div(null,
+    MyTitleFact({title: 'Props are great!', color: 'mediumaquamarine'})
+  )
+)
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
