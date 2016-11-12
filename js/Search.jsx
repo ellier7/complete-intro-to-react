342ff9067157d57a194c14fc7d86f776aa807cfe
ellier7 <eroussopoulos@gmail.com>
2016-11-12 12:08:55 -0500

    Search bar added

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,18 +2,22 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <div className='shows'>
+          {
     data.shows.map((show) => (
       <ShowCard
         {...show} // instead of passing everything individually
         key={show.imdbID}
       />
     ))}
-    </div>
-  </div>
+        </div>
+      </div>
   )
+  }
+})
 
 module.exports = Search
