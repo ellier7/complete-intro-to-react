ddc7b5cfbd929aa51cbc6dd343620aaecce72e2e
ellier7 <eroussopoulos@gmail.com>
2016-11-12 22:30:07 -0500

    Can search from landing page

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,40 @@
 const React = require('react')
+const { hashHistory } = require('react-router')
 const { Link } = require('react-router')
-// stateless component (just a function)
-  // can use function express since youre only returning divs
-  // use return if you want conditionals and variables
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to='/search' className='browse-all'>Or Browse All</Link>
-  </div>
-)
+const { connector } = require('./Store')
 
-module.exports = Landing
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+    this.gotoSearch = this.gotoSearch.bind(this)
+  }
+  handleSearchTermEvent (event) {
+    this.props.setSearchTerm(event.target.value)
+  }
+  gotoSearch (event) {
+    hashHistory.push('search')
+    event.preventDefault() // prevents from moving to another page
+  }
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <form onSubmit={this.gotoSearch}>
+          <input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='search' type='text' placeholder='Search' />
+        </form>
+        <Link to='/search' className='browse-all'> or Browse All</Link>
+      </div>
+    )
+  }
+}
+
+const { func, string } = React.PropTypes
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
+
+module.exports = connector(Landing)
